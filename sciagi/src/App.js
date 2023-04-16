7766dde43adee30dd8f1ce17e6ace2735e083858
Gorbkos <gniewkoscielak@gmail.com>
2023-04-16 21:09:08 +0200

    abc

--- sciagi/src/App.js
+++ sciagi/src/App.js
@@ -3,22 +3,56 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <header>
+          <h2>Historia</h2>
+        </header>
+        <div class="menu">
+          <a href="index.html">Strona główna</a>
+          <a href="Historia.html">Historia</a>
+          <a href="Chemia.html">Chemia</a>
+          <a href="BIologia.html">Biologia</a>
+        </div>
+
+        <section class="main-section">
+          <article>
+            <h2>Pełnia i schyłek średniowiecza</h2>
+          </article>
+          <h1>Wydarzenia</h1>
+          <ul>
+            <li>1054 r. - schizma wschodnia</li>
+            <li>1077 r. - Henryk IV ukorzył się przed papieżem w Canossie</li>
+            <li>1095 r. -synod w Clermont</li>
+            <li>1096 r. - początek pierwszej krucjaty</li>
+            <li>1122 r. - Konkordat Wormariski</li>
+            <li>1202 - Początek IV krucjaty</li>
+            <li>1206 - Temudzyn przyjmuje tytuł Gengiscan</li>
+            <li>1209-założył zakon franciszkanów</li>
+            <li>1216- założył zakon dominikanów</li>
+            <li>1240 r. - podbój Rusi przez Mongolów</li>
+            <li>1241 r. - bitwa pod Legnica</li>
+            <li>1291 r.- upadek Akki/koniec wypraw krzyżowych</li>
+            <li>1337 r. - wybuch wojny stuletniej</li>
+            <li>1346 r. - bitwa pod Crécy</li>
+            <li>1347 r. - początek epidemii dżumy</li>
+            <li>1358 - powstanie ludowe - tzw. akeria we Francji</li>
+            <li>1378 - początek góralszczyzny</li>
+            <li>1381-powstanie pod Wat Tyler w Anglii</li>
+            <li>1389 - bitwa pod Kosowem Polem</li>
+            <li>1414-1418-sobór w Konstancji</li>
+            <li>1415 r. - bitwa pod Azincourt</li>
+            <li>1415 r. - spalenie Jana Husa na stosie</li>
+            <li>1434 r.-bitwa pod Lipanami</li>
+            <li>1444 r. - bitwa pod Warną</li>
+            <li>1453 r.- zdobycie Konstantynopola przez Turków</li>
+            <li>1453 r. - zakończenie wojny stuletniej</li>
+          </ul>
+        </section>
+
+        <footer>&copy; 2022 Gniewko Kościelak</footer>
+      </div>
+    </body>
   );
 }
 
